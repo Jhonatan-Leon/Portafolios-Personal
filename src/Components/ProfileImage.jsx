540f3e0bce1c4db8321b599a47996e2db1e7f24d
jonathan cardona <c.jhonatan2007@gmail.com>
2025-03-19 22:50:27 -0500

    bug: Error cargar la imagen

--- src/Components/ProfileImage.jsx
+++ src/Components/ProfileImage.jsx
@@ -4,7 +4,7 @@
 export const ProfileImage = () => {
     return (
         <div className="w-52 h-52 md:w-60 md:h-60 rounded-full overflow-hidden shadow-lg">
-            <img src="src\assets\Perfil.jpeg" alt="Foto de perfil" className="object-cover w-full h-full" />
+            <img src="src/assets/Perfil.jpeg" alt="Foto de perfil" className="object-cover w-full h-full" />
         </div>
     );
 }
